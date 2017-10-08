2ed2e3d974003160de6475e911513c8b0028b4de
Untamanei <31429630+Untamanei@users.noreply.github.com>
2017-10-08 15:25:39 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,7 @@
 curl -X POST \
 -H 'Content-Type:application/json' \
--H 'Authorization: Bearer {ENTER_ACCESS_TOKEN}' \
+-H 'Authorization: Bearer +LtQfr5HpAMhtJuE2VBQxoXiFARCnVXn3DNE8LkmGHTruOUXcxWIxh+IOBNhy2lQ8Q/nAbQdyX+yPeOtQ/GcvQQFH122gT6Lu0eqtci22BQjA48JGV9RJ3B8Rbi34ZiHy8+OUKms2JIlOT9hjVrAtAdB04t89/1O/w1cDnyilFU=
+' \
 -d '{
     "replyToken":"nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
     "messages":[
