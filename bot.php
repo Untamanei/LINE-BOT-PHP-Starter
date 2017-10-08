cd46d1a34121afd9a0b8abf980ce52f3a6c0edb9
Untamanei <31429630+Untamanei@users.noreply.github.com>
2017-10-08 12:16:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,9 +18,11 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => "Hello world"
+				//'type' => 'text',
+				//'text' => "Hello world"
 				//'text' => $text
+				"type": "text",
+    				"text": "\uDBC0\uDC84 LINE emoji"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
