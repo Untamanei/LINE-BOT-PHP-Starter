142cdd98144cba7c014b9494adea42b0ad0226f9
Untamanei <31429630+Untamanei@users.noreply.github.com>
2017-10-08 19:25:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -6,6 +6,12 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
+$strUrl = "https://api.line.me/v2/bot/message/reply";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
