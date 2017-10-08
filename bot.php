8ee0733077168bbee855e95ca821c889fbad5f66
Untamanei <31429630+Untamanei@users.noreply.github.com>
2017-10-08 20:21:43 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -35,11 +35,15 @@ $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
 	  $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 	}
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL,$strUrl);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$result = curl_exec($ch);
+		curl_close ($ch);
+ 
+?>
