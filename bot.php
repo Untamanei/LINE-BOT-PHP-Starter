a9189ff775829efc2d55a9c92411176286f92fb8
Untamanei <31429630+Untamanei@users.noreply.github.com>
2017-10-08 19:27:33 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -46,8 +46,8 @@ if (!is_null($events['events'])) {
 			$result = curl_exec($ch);
 			curl_close($ch);
 
-			echo $result . "\r\n";
+			//echo $result . "\r\n";
 		}
 	}
 }
-echo "OK";
+//echo "OK";
