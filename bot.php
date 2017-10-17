07059b57896781e8608d9e9dec647b4c65394824
Untamanei <31429630+Untamanei@users.noreply.github.com>
2017-10-17 22:56:48 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,30 +1,30 @@
-$post = file_get_contents(‘php://input’);
-$urlReply = ‘https://api.line.me/v2/bot/message/reply';
-$token = ‘qkemiHYLq1Go2vUvCixhJFKeTfxGgxd57LkiruFfPV58idgTkUbL7Tn7HUtluFRI8Q/nAbQdyX+yPeOtQ/GcvQQFH122gT6Lu0eqtci22BQ2/doT8zSakFQ7jLIfeakP0NGR6NLOqMZPM+y71siy6wdB04t89/1O/w1cDnyilFU=’;
+<?php
 
-{“events”:[{“type”:”message”,”replyToken”:”ไม่บอก”,”source”:{“userId”:”ไม่บอก”,”type”:”user”},”timestamp”:1477132643802,”message”:{“type”:”text”,”id”:”5094630491076",”text”:”ว่าไงท่าน”}}]}
+$post = file_get_contents('php://input');
+$urlReply = 'https://api.line.me/v2/bot/message/reply';
+$token = 'qkemiHYLq1Go2vUvCixhJFKeTfxGgxd57LkiruFfPV58idgTkUbL7Tn7HUtluFRI8Q/nAbQdyX+yPeOtQ/GcvQQFH122gT6Lu0eqtci22BQ2/doT8zSakFQ7jLIfeakP0NGR6NLOqMZPM+y71siy6wdB04t89/1O/w1cDnyilFU=';
 
 function getSticker($replyToken){
  $sticker = array(
- ‘type’ => ‘sticker’,
- ‘packageId’ => ‘4’,
- ‘stickerId’ => ‘300’
+ 'type' => 'sticker',
+ 'packageId' => '4',
+ 'stickerId' => '300'
  );
  $packet = array(
- ‘replyToken’ => $replyToken,
- ‘messages’ => array($sticker),
+ 'replyToken' => $replyToken,
+ 'messages' => array($sticker),
  );
  return $packet;
 }
 
-$packet = getSticker($item[‘replyToken’]);
+$packet = getSticker($item['replyToken']);
  postMessage($token,$packet,$urlReply);
 
 function postMessage($token,$packet,$urlReply){
  $dataEncode = json_encode($packet);
- $headersOption = array(‘Content-Type: application/json’,’Authorization: Bearer ‘.$token);
+ $headersOption = array('Content-Type: application/json','Authorization: Bearer '.$token);
  $ch = curl_init($urlReply);
- curl_setopt($ch,CURLOPT_CUSTOMREQUEST,’POST’);
+ curl_setopt($ch,CURLOPT_CUSTOMREQUEST,'POST');
  curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
  curl_setopt($ch,CURLOPT_POSTFIELDS,$dataEncode);
  curl_setopt($ch,CURLOPT_HTTPHEADER,$headersOption);
@@ -33,3 +33,4 @@ function postMessage($token,$packet,$urlReply){
  curl_close($ch);
 }
 
+?>
