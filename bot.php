d1d5734b272492c4fefa60a162a8266536e93e73
Untamanei <31429630+Untamanei@users.noreply.github.com>
2017-10-17 22:37:31 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,7 +21,7 @@ if($arrJson['events'][0]['message']['text'] == "Wi PRU"){
   else if($arrJson['events'][0]['message']['text'] == "test"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['type'] = "sticker";
   $arrPostData['messages'][0]['packageId'] = "1";
    $arrPostData['messages'][0]['stickerId'] = "1";
   }
